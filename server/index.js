4c19a52c25c870e7c9339752b1b73d37933156de
mokambujhb024 <mokambujhb024@student.wethinkcode.co.za>
2025-09-11 10:05:41 +0200

    Restructured project into client/server setup with express

---
+++ server/index.js
@@ -0,0 +1,22 @@
+// server/index.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
