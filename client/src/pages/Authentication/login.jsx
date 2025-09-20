12188b12fcd217a1c709c2870fd6bf432de22886
mokambujhb024 <mokambujhb024@student.wethinkcode.co.za>
2025-09-20 23:45:51 +0200

    creating login page

--- client/src/pages/Authentication/login.jsx
+++ client/src/pages/Authentication/login.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+
+import styles from './loging.module.css';
+import React, { useState } from 'react';
+
 
 function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return(
-        <div>Login</div>
+        <div className={styles.login_card}>
+            <div>
+                <h4>Login</h4>
+                <div className={styles.input_wrapper}>
+                    <input placeholder="username"
+                    value={username}
+                    onChange={(e)=>setUsername(e.target.value)}/>
+                </div>
+                 <div className={styles.input_wrapper}>
+                    <input placeholder="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}/>
+                </div>
+            </div>
+        </div>
     )
 }
 
