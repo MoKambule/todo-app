bb30099c838c4a12757cb1b8d925b78a5f7def85
mokambujhb024 <mokambujhb024@student.wethinkcode.co.za>
2025-09-29 16:32:54 +0200

    completing list routes and controllers

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -11,3 +11,41 @@ exports.createToDo = async (req,res)=>{
         console.log(err);
         res.status(err);
     }}
+
+    exports.getAllToDo = async (req,res)=>{
+        let {userId} = req.params;
+
+        try{
+            const result = await ToDo.find({createdby:userId});
+            res.send(result);
+        }catch(err){
+            console.log(err);
+            res.status(400).send(err);
+        }
+    }
+ 
+       exports.deleteToDo = async (req,res)=>{
+
+        try{
+            const {id} = req.params;
+            const result = await ToDo.findByIdAndDelete(id);
+            res.send({message: 'ToDo Task Deleted!'});
+        }catch(err){
+            console.log(err);
+            res.status(400).send(err);
+        }
+    }
+
+       exports.updateToDo = async (req,res)=>{
+
+        try{
+             const {id} = req.params;
+             const data = req.body;
+            const result = await ToDo.findByIdAndUpdate(id,{$set:data},{returnOriginal:false} );
+            rconsole.log(result);
+            ews.send({message:'ToDo list Updated!'});
+        }catch(err){
+            console.log(err);
+            res.status(400).send(err);
+        }
+    }
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const {createToDo} = require('../controllers/todoControllers');
+const {createToDo, getAllToDo, deleteToDo,updateToDo} = require('../controllers/todoControllers');
 const router = express.Router();
 
 router.post('/create-to-do', createToDo);
+router.get('/get-all-to-do/userId', authenticateToken,getAllToDo);
+router.get('/delete-to-do/userId', authenticateToken,deleteToDo);
+router.get('/update-to-do/userId', authenticateToken,updateToDo);
 
 
 
